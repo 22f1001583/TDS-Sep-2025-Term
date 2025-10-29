9181b44c826cd18816828e821af378624e105087
Kartikey <22f1001583@ds.study.iitm.ac.in>
2025-10-30 00:46:34 +0530

    Refactor tests for longest_positive_streak function

--- tests/test_streak.py
+++ tests/test_streak.py
@@ -1,29 +1,15 @@
+# tests/test_streak.py
 import pytest
 from streak import longest_positive_streak
+
+def test_basic_streak():
+    assert longest_positive_streak([1, 2, 3, -1, 4, 5]) == 3
 
 def test_empty_list():
     assert longest_positive_streak([]) == 0
 
-def test_no_positive_numbers():
-    assert longest_positive_streak([-1, -2, 0, -5]) == 0
+def test_no_positive():
+    assert longest_positive_streak([-1, -2, -3]) == 0
 
-def test_all_positive_numbers():
+def test_all_positive():
     assert longest_positive_streak([1, 2, 3, 4, 5]) == 5
-
-def test_single_streak():
-    assert longest_positive_streak([2, 3, -1, 5, 6, 7, 0, 4]) == 3
-
-def test_multiple_streaks_longest_last():
-    assert longest_positive_streak([1, 2, 0, 1, 2, 3]) == 3
-
-def test_multiple_streaks_longest_first():
-    assert longest_positive_streak([1, 2, 3, 0, 1, 2]) == 3
-
-def test_streaks_with_zeros_and_negatives():
-    assert longest_positive_streak([1, 0, 2, 3, -4, 5, 6, 7, 8]) == 4
-
-def test_single_element_streaks():
-    assert longest_positive_streak([1, -1, 2, -2, 3, -3]) == 1
-
-def test_long_list():
-    assert longest_positive_streak([1] * 1000) == 1000
